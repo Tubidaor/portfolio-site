a618853e6273de7f6bea9a2be2888527eb90c5e8
Juan Baltazar <juan.baltazar1@gmail.com>
2019-08-28 23:27:33 -0700

    added hamburger functionality and all styling

---
+++ index.js
@@ -0,0 +1,6 @@
+function onClickMenu() {
+	document.getElementById("hamburger").classList.toggle("change");
+	document.getElementById("menu").classList.toggle("change");
+	
+	document.getElementById("menu-bg").classList.toggle("change-bg");
+}
