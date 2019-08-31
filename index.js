6c251e0a575186707e5683fbed1228cadb23ef19
Juan Baltazar <juan.baltazar1@gmail.com>
2019-08-31 13:50:52 -0700

    updated links fixed menu

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 function onClickMenu() {
 	document.getElementById("hamburger").classList.toggle("change");
 	document.getElementById("menu").classList.toggle("change");
-	
 	document.getElementById("menu-bg").classList.toggle("change-bg");
 }
